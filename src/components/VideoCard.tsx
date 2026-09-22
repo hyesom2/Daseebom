a0e332b72b18d15e693a1fc45cf109f1025a34cb
kimHyeonJu <kimhyesom1202@gmail.com>
2026-09-22 15:55:05 +0900

    Feat : 썸네일 클릭 시 재생 기능 추가
    - 썸네일 클릭 시 동영상이 toggle형식으로 나타나도록 구현
    - X 아이콘으로 닫기 기능
    - 원본 보기 링크 추가

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,18 +1,58 @@
 import type { YouTubeVideo } from '@/types/youtube';
+import { useState } from 'react';
 
 type VideoCardProps = {
   video: YouTubeVideo;
 };
 
 export default function VideoCard({ video }: VideoCardProps) {
-  return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} alt="" />
+  const [isOpen, setIsOpen] = useState(false);
 
-      <div>
-        <h3>{video.snippet.title}</h3>
-        <p>{video.snippet.channelTitle}</p>
-      </div>
-    </div>
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <article className="flex flex-col">
+      <button type="button" onClick={handleToggle} aria-expanded={isOpen}>
+        <img src={video.snippet.thumbnails.medium.url} alt="" />
+
+        <div>
+          <h3>{video.snippet.title}</h3>
+          <p>{video.snippet.channelTitle}</p>
+        </div>
+      </button>
+      {isOpen && (
+        <div id={`video-player-${video.id}`}>
+          <div className="relative w-full aspect-video">
+            <iframe
+              src={`https://www.youtube.com/embed/${video.id}`}
+              title={video.snippet.title}
+              className="absolute insert-0 w-full h-full"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+            <button
+              type="button"
+              onClick={handleClose}
+              className="absolute top-2 right-2 w-6 h-6 z-100 text-white font-bold"
+              aria-label="동영상 닫기"
+            >
+              X
+            </button>
+          </div>
+          <a
+            href={`https://www.youtube.com/watch?v=${video.id}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            원본 보기
+          </a>
+        </div>
+      )}
+    </article>
   );
 }
